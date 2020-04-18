9b57b7ad70012b9faf98974cf804ea1ebc6cbf76
Alexei Udarbe <alexeiudarbe@AU-MacBook-Bro.local>
2020-04-18 14:39:15 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,20 @@
-let cardOne = 'Queen';
-let TwoOne = 'King';
-let cardThree = 'Queen';
-let cardFour = 'King';
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardFour);
+let cards = ['queen', 'queen', 'king', 'king'];
+let cardsInPlay = [];
+let cardOne = cards[0];
+let cardTwo = cards[2];
+
+
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
+console.log('User flipped: '+ cardOne);
+console.log('User flipped: '+ cardTwo);
+
+if (cardsInPlay.length === 2){
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	} else {
+		alert('Sorry, try again!')
+	}
+}
