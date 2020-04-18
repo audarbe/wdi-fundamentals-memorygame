3ca624c18984e4c2ffd762035c552fdebd4c51ed
Alexei Udarbe <alexeiudarbe@AU-MacBook-Bro.local>
2020-04-18 16:47:26 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,20 +1,22 @@
 
 let cards = ['queen', 'queen', 'king', 'king'];
 let cardsInPlay = [];
-let cardOne = cards[0];
-let cardTwo = cards[2];
 
+function checkForMatch() {
+	if (cardsInPlay.length === 2){
+		if (cardsInPlay[0] === cardsInPlay[1]) {
+			console.log("You found a match!");
+		} else {
+			console.log('Sorry, try again!')
+		};
+	};
+};
 
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
+function flipCard(cardId) {
+	console.log("User Flipped: " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	checkForMatch();
+};
 
-console.log('User flipped: '+ cardOne);
-console.log('User flipped: '+ cardTwo);
-
-if (cardsInPlay.length === 2){
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match!");
-	} else {
-		alert('Sorry, try again!')
-	}
-}
+flipCard(0);
+flipCard(1);
